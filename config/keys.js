d436fecb6385b24ed9f655d87935456d4a1f7237
garrison1234 <handrade88@gmail.com>
2018-06-19 16:04:52 -0700

    JWT test

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://Hecto:starcraft1@ds259620.mlab.com:59620/devconnector'
-}
+  mongoURI: 'mongodb://Hecto:starcraft1@ds259620.mlab.com:59620/devconnector',
+  secretOrKey: 'secret'
+};
